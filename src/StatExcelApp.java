2d01b508066ddf745a95caadcd9358871e837817
OSAndreev <andreevos22@mail.ru>
2024-06-29 21:54:17 +0300

    make code cleaner

--- src/StatExcelApp.java
+++ src/StatExcelApp.java
@@ -102,11 +102,4 @@ public class StatExcelApp extends JFrame {
 
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new StatExcelApp().setVisible(true);
-            }
-        });
-    }
 }
