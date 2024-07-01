2acb02bf398284bdb7b1abf3770861723472275c
OSAndreev <andreevos22@mail.ru>
2024-07-01 03:11:14 +0300

    add files

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        // Используем метод invokeLater для того, чтобы убедиться, что GUI-интерфейс
-        // создается и обрабатывается в потоке обработки событий (EDT).
+
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                // Создаем экземпляр главного окна приложения
                 StatExcelApp app = new StatExcelApp();
-                // Устанавливаем видимость окна в true, чтобы оно отобразилось
                 app.setVisible(true);
             }
         });
